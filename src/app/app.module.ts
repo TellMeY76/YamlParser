872ceb0d6d6f7a9dd47ebada068e4119097e64d3
TellMeY76 <895793323@qq.com>
2020-02-26 10:40:30 +0800

    test js-yaml

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
+// ng
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import { MultiSelectModule } from 'primeng/multiselect'
-import {DropdownModule} from 'primeng/dropdown';
-import {CardModule} from 'primeng/card';
-import {TableModule} from 'primeng/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// primeng
+import { MultiSelectModule } from 'primeng/multiselect'
+import { DropdownModule } from 'primeng/dropdown';
+import { CardModule } from 'primeng/card';
+import { TableModule } from 'primeng/table';
+import { PanelModule } from 'primeng/panel';
+import { TabViewModule } from 'primeng/tabview';
+import { StepsModule } from 'primeng/steps';
+import { InputTextModule } from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+
 import { AppComponent } from './app.component';
 import { CaseEditComponent } from './component/case-edit/case-edit.component';
 
@@ -17,11 +26,17 @@ import { CaseEditComponent } from './component/case-edit/case-edit.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
     MultiSelectModule,
     DropdownModule,
     TableModule,
     CardModule,
+    PanelModule,
+    StepsModule,
+    TabViewModule,
+    InputTextModule,
+    ButtonModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/component/case-edit/case-edit.component.ts
+++ src/app/component/case-edit/case-edit.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { SelectItem } from 'primeng/api';
+import { MenuItem } from 'primeng/api';
 
 interface City {
   name: string,
   code: string
 }
+
 @Component({
   selector: 'app-case-edit',
   templateUrl: './case-edit.component.html',
-  styleUrls: ['./case-edit.component.sass']
+  styleUrls: ['./case-edit.component.scss']
 })
 export class CaseEditComponent implements OnInit {
+  stepItems: MenuItem[] = [
+    { label: '新建分析器' },
+    { label: '添加任务' },
+    { label: '完成' }
+  ];
 
   cities1: SelectItem[];
 
@@ -19,6 +26,7 @@ export class CaseEditComponent implements OnInit {
   selectedCities1: City[];
 
   selectedCities2: City[];
+
   constructor() {
     this.cities1 = [
       { label: 'New York', value: { id: 1, name: 'New York', code: 'NY' } },
@@ -39,6 +47,16 @@ export class CaseEditComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    const yaml = require('js-yaml');
+    const jsonObj = { type: '1', arr: [{ user: 'ww', age: 18, firiends: [{ name: 'we', age: 111 }, { name: 'we', age: 111 }, { name: 'we', age: 111 }] }, { user: 'wtj', age: 24 }] }
+    try {
+      const yamldata = yaml.safeDump(jsonObj)
+      console.log(yamldata);
+    } catch (e) {
+      console.error(e)
+    }
   }
-
+  addCondition() {
+    console.log('click')
+  }
 }
